d2a43c3bee2018181cabbb2fa79aa0343172324d
Ritik Raj <raj769417@gmail.com>
2024-07-20 02:30:52 +0530

    feat: run different code based on the type of data: text ot file

--- dist/controllers/data.js
+++ dist/controllers/data.js
@@ -17,12 +17,35 @@ const fs_1 = __importDefault(require("fs"));
 const file_upload_helper_1 = require("../utils/file-upload-helper");
 const textLoaders_1 = require("../genai/textLoaders");
 const analyzeData_1 = require("../genai/analyzeData");
+const ApiError_1 = require("../utils/ApiError");
 const fetchDataAnalytics = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const tempFilePath = yield (0, file_upload_helper_1.getTempSavedFilePath)(req);
-    const textDocument = yield (0, textLoaders_1.loadText)(tempFilePath);
-    const textContent = textDocument[0].pageContent;
+    var _a, _b, _c;
+    if (!req.file && !((_a = req.body) === null || _a === void 0 ? void 0 : _a.text)) {
+        throw new ApiError_1.ApiError(400, "Provide a valid file or text");
+    }
+    const type_of_data = (_b = req.body) === null || _b === void 0 ? void 0 : _b.type_of_data;
+    if (!type_of_data) {
+        throw new ApiError_1.ApiError(400, "Invalid data received");
+    }
+    let textContent;
+    let tempFilePath;
+    if (type_of_data === "file") {
+        if (!req.file) {
+            throw new ApiError_1.ApiError(400, "File not provided");
+        }
+        tempFilePath = yield (0, file_upload_helper_1.getTempSavedFilePath)(req);
+        const textDocument = yield (0, textLoaders_1.loadText)(tempFilePath);
+        textContent = textDocument[0].pageContent;
+    }
+    else {
+        if (!((_c = req.body) === null || _c === void 0 ? void 0 : _c.text)) {
+            throw new ApiError_1.ApiError(400, "Text not provided");
+        }
+        textContent = req.body.text;
+    }
     const { summary, sentiment, keywords } = yield (0, analyzeData_1.analyzeData)(textContent);
-    yield fs_1.default.promises.unlink(tempFilePath);
+    if (type_of_data === "file" && tempFilePath)
+        yield fs_1.default.promises.unlink(tempFilePath);
     res.status(200).json({
         success: true,
         data: {
--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -4,15 +4,40 @@ import { Request, Response } from "express";
 import { getTempSavedFilePath } from "../utils/file-upload-helper";
 import { loadText } from "../genai/textLoaders";
 import { analyzeData } from "../genai/analyzeData";
+import { ApiError } from "../utils/ApiError";
 
 const fetchDataAnalytics = async (req: Request, res: Response) => {
-    const tempFilePath = await getTempSavedFilePath(req);
-    const textDocument = await loadText(tempFilePath);
-    const textContent = textDocument[0].pageContent;
+    if (!req.file && !req.body?.text) {
+        throw new ApiError(400, "Provide a valid file or text");
+    }
+
+    const type_of_data = req.body?.type_of_data;
+    if (!type_of_data) {
+        throw new ApiError(400, "Invalid data received");
+    }
+
+    let textContent;
+    let tempFilePath;
+
+    if (type_of_data === "file") {
+        if (!req.file) {
+            throw new ApiError(400, "File not provided");
+        }
+
+        tempFilePath = await getTempSavedFilePath(req);
+        const textDocument = await loadText(tempFilePath);
+        textContent = textDocument[0].pageContent;
+    } else {
+        if (!req.body?.text) {
+            throw new ApiError(400, "Text not provided");
+        }
+        textContent = req.body.text;
+    }
 
     const { summary, sentiment, keywords } = await analyzeData(textContent);
 
-    await fs.promises.unlink(tempFilePath);
+    if (type_of_data === "file" && tempFilePath)
+        await fs.promises.unlink(tempFilePath);
 
     res.status(200).json({
         success: true,
