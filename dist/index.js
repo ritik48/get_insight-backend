fd8c2cef834200f423fb05cc71eabb5ad3dbecf5
Ritik Raj <raj769417@gmail.com>
2024-07-19 21:21:29 +0530

    feat: setup multer and analzed data from file upload

--- dist/index.js
+++ dist/index.js
@@ -16,12 +16,37 @@ const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const express_1 = __importDefault(require("express"));
 const analyzeData_1 = require("./genai/analyzeData");
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = process.env.PORT || 4000;
-app.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { text } = req.body;
-    const { summary, sentiment, keywords } = yield (0, analyzeData_1.analyzeData)(text);
+const multer_1 = __importDefault(require("multer"));
+const allowedFileTypes = [".txt", ".pdf", ".doc", ".docx"];
+const path_1 = __importDefault(require("path"));
+const file_upload_helper_1 = require("./utils/file-upload-helper");
+const textLoaders_1 = require("./genai/textLoaders");
+const storage = multer_1.default.memoryStorage();
+const upload = (0, multer_1.default)({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const extension = path_1.default.extname(file.originalname);
+        if (allowedFileTypes.includes(extension.toLowerCase())) {
+            cb(null, true);
+        }
+        else {
+            cb(new Error("Ony these file types are allowed : " +
+                allowedFileTypes.join(", ")));
+        }
+    },
+});
+app.post("/", upload.single("file_data"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const tempFilePath = yield (0, file_upload_helper_1.getTempSavedFilePath)(req);
+    const textDocument = yield (0, textLoaders_1.loadText)(tempFilePath);
+    const textContent = textDocument[0].pageContent;
+    const { summary, sentiment, keywords } = yield (0, analyzeData_1.analyzeData)(textContent);
+    yield fs_1.default.promises.unlink(tempFilePath);
     res.status(200).json({
         success: true,
         data: {
@@ -38,6 +63,7 @@ app.use((err, req, res, next) => {
     const { status = 500, message } = err;
     res.status(status).json({ success: false, message });
 });
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log("Listening on port ", PORT);
 });
--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,65 @@ import express, {
     Response,
 } from "express";
 import { analyzeData } from "./genai/analyzeData";
-import { isAuthenticated } from "./utils/midllewares";
+
+import os from "os";
+import fs from "fs";
 
 const app = express();
-
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+import multer from "multer";
 
-app.post("/", async (req: Request, res: Response) => {
-    const { text } = req.body;
+const allowedFileTypes = [".txt", ".pdf", ".doc", ".docx"];
+import path from "path";
+import { ApiError } from "./utils/ApiError";
+import { getTempSavedFilePath } from "./utils/file-upload-helper";
+import { loadText } from "./genai/textLoaders";
 
-    const { summary, sentiment, keywords } = await analyzeData(text);
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const extension = path.extname(file.originalname);
 
-    res.status(200).json({
-        success: true,
-        data: {
-            summary: summary.content,
-            sentiment: sentiment.content,
-            keywords: keywords.content,
-        },
-    });
+        if (allowedFileTypes.includes(extension.toLowerCase())) {
+            cb(null, true);
+        } else {
+            cb(
+                new Error(
+                    "Ony these file types are allowed : " +
+                        allowedFileTypes.join(", ")
+                )
+            );
+        }
+    },
 });
+
+app.post(
+    "/",
+    upload.single("file_data"),
+    async (req: Request, res: Response) => {
+        const tempFilePath = await getTempSavedFilePath(req);
+        const textDocument = await loadText(tempFilePath);
+        const textContent = textDocument[0].pageContent;
+
+        const { summary, sentiment, keywords } = await analyzeData(textContent);
+
+        await fs.promises.unlink(tempFilePath);
+
+        res.status(200).json({
+            success: true,
+            data: {
+                summary: summary.content,
+                sentiment: sentiment.content,
+                keywords: keywords.content,
+            },
+        });
+    }
+);
 
 app.get("/status", (req: Request, res: Response) => {
     res.json({ success: true, message: "server online" });
@@ -40,6 +77,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(status).json({ success: false, message });
 });
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log("Listening on port ", PORT);
 });
