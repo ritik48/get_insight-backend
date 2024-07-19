8818843775ce87ffd5d3ff16b309dfa2ced37540
Ritik Raj <raj769417@gmail.com>
2024-07-19 22:01:28 +0530

    refactor: moved the the routes function to their own file

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,48 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const express_1 = __importDefault(require("express"));
-const analyzeData_1 = require("./genai/analyzeData");
-const fs_1 = __importDefault(require("fs"));
+const data_1 = require("./routes/data");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const multer_1 = __importDefault(require("multer"));
-const allowedFileTypes = [".txt", ".pdf", ".doc", ".docx"];
-const path_1 = __importDefault(require("path"));
-const file_upload_helper_1 = require("./utils/file-upload-helper");
-const textLoaders_1 = require("./genai/textLoaders");
-const storage = multer_1.default.memoryStorage();
-const upload = (0, multer_1.default)({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024,
-    },
-    fileFilter: (req, file, cb) => {
-        const extension = path_1.default.extname(file.originalname);
-        if (allowedFileTypes.includes(extension.toLowerCase())) {
-            cb(null, true);
-        }
-        else {
-            cb(new Error("Ony these file types are allowed : " +
-                allowedFileTypes.join(", ")));
-        }
-    },
-});
-app.post("/", upload.single("file_data"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const tempFilePath = yield (0, file_upload_helper_1.getTempSavedFilePath)(req);
-    const textDocument = yield (0, textLoaders_1.loadText)(tempFilePath);
-    const textContent = textDocument[0].pageContent;
-    const { summary, sentiment, keywords } = yield (0, analyzeData_1.analyzeData)(textContent);
-    yield fs_1.default.promises.unlink(tempFilePath);
-    res.status(200).json({
-        success: true,
-        data: {
-            summary: summary.content,
-            sentiment: sentiment.content,
-            keywords: keywords.content,
-        },
-    });
-}));
-app.get("/status", (req, res) => {
+app.use("/data", data_1.router);
+app.get("/", (req, res) => {
     res.json({ success: true, message: "server online" });
 });
 app.use((err, req, res, next) => {
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,14 @@
 import { config } from "dotenv";
 config();
-import express, {
-    NextFunction,
-    Request,
-    RequestHandler,
-    Response,
-} from "express";
-import { analyzeData } from "./genai/analyzeData";
-
-import os from "os";
-import fs from "fs";
+import express, { NextFunction, Request, Response } from "express";
+import { router as dataRouter } from "./routes/data";
 
 const app = express();
 app.use(express.json());
 
-import multer from "multer";
+app.use("/data", dataRouter);
 
-const allowedFileTypes = [".txt", ".pdf", ".doc", ".docx"];
-import path from "path";
-import { ApiError } from "./utils/ApiError";
-import { getTempSavedFilePath } from "./utils/file-upload-helper";
-import { loadText } from "./genai/textLoaders";
-
-const storage = multer.memoryStorage();
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024,
-    },
-    fileFilter: (req, file, cb) => {
-        const extension = path.extname(file.originalname);
-
-        if (allowedFileTypes.includes(extension.toLowerCase())) {
-            cb(null, true);
-        } else {
-            cb(
-                new Error(
-                    "Ony these file types are allowed : " +
-                        allowedFileTypes.join(", ")
-                )
-            );
-        }
-    },
-});
-
-app.post(
-    "/",
-    upload.single("file_data"),
-    async (req: Request, res: Response) => {
-        const tempFilePath = await getTempSavedFilePath(req);
-        const textDocument = await loadText(tempFilePath);
-        const textContent = textDocument[0].pageContent;
-
-        const { summary, sentiment, keywords } = await analyzeData(textContent);
-
-        await fs.promises.unlink(tempFilePath);
-
-        res.status(200).json({
-            success: true,
-            data: {
-                summary: summary.content,
-                sentiment: sentiment.content,
-                keywords: keywords.content,
-            },
-        });
-    }
-);
-
-app.get("/status", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.json({ success: true, message: "server online" });
 });
 
