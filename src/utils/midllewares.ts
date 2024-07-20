f0b07e9a5ccb108b3039f5b3dd3ec167a44be93c
Ritik Raj <raj769417@gmail.com>
2024-07-21 01:59:36 +0530

    feat: verify jwt received from the frontend

--- src/utils/midllewares.ts
+++ src/utils/midllewares.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "./ApiError";
+import { verifyJwt } from "./verifyJwt";
 
 const isAuthenticated = async (
     req: Request,
@@ -9,17 +10,23 @@ const isAuthenticated = async (
     try {
         const authorization_header = req.headers["authorization"];
         if (!authorization_header) {
-            throw new ApiError(401, "User not authenticated");
+            throw new ApiError(401, "You don't have the required access.");
         }
 
         const token = authorization_header.split(" ")[1];
-
         if (!token) {
-            throw new ApiError(401, "User not authenticated");
+            throw new ApiError(401, "You don't have the required access.");
         }
-        console.log("token = ", token);
+
+        try {
+            await verifyJwt(token);
+        } catch (error) {
+            throw new ApiError(400, "You don't have the required access.");
+        }
+
         next();
     } catch (error) {
+        console.log("error = ", error);
         next(error);
     }
 };
