f5e68d3ebb4f67ae41e6b00ed7486f2ad5f36683
Ritik Raj <raj769417@gmail.com>
2024-07-21 02:00:08 +0530

    feat: include middleware in the route

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -2,9 +2,15 @@ import express from "express";
 import { upload } from "../utils/file-upload-helper";
 import { fetchDataAnalytics } from "../controllers/data";
 import { catchAsync } from "../utils/catchAsync";
+import { isAuthenticated } from "../utils/midllewares";
 
 const router = express.Router();
 
-router.post("/", upload.single("file_data"), catchAsync(fetchDataAnalytics));
+router.post(
+    "/",
+    catchAsync(isAuthenticated),
+    upload.single("file_data"),
+    catchAsync(fetchDataAnalytics)
+);
 
 export { router };
