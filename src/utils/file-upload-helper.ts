9cef16955b1ded5aeddacc5df9ac95f9a9842817
Ritik Raj <raj769417@gmail.com>
2024-07-20 19:44:49 +0530

    feat: initialise cloudinary and  function to upload file to it

--- src/utils/file-upload-helper.ts
+++ src/utils/file-upload-helper.ts
@@ -3,11 +3,22 @@ import fs from "fs";
 import path from "path";
 import { Request } from "express";
 import multer from "multer";
+import { Readable } from "stream";
+
+// Configure Cloudinary
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+cloudinary.config({
+    secure: true,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+});
 
 import { ApiError } from "./ApiError";
 
 const allowedFileTypes = [".txt", ".pdf", ".doc", ".docx"];
 
+// Configure Multer
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
@@ -43,4 +54,23 @@ const getTempSavedFilePath = async (req: Request) => {
     }
 };
 
-export { getTempSavedFilePath, upload };
+const uploadToCloudinary = (
+    req: Request
+): Promise<UploadApiResponse | undefined> => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                resource_type: "auto",
+                folder: "ai",
+                filename_override: req.file?.originalname,
+            },
+            (error, result) => {
+                if (error) reject(error);
+                else resolve(result);
+            }
+        );
+        Readable.from(req.file?.buffer as Buffer).pipe(stream);
+    });
+};
+
+export { getTempSavedFilePath, upload, uploadToCloudinary };
